c24e32d4e8e39c75f0e07e3d7e307bb33a8c899a
Deoshor <39273195+Deoshor@users.noreply.github.com>
2019-08-04 16:38:49 +0300

    upd1

--- Less1_idealweight .rb
+++ Less1_idealweight .rb
@@ -4,10 +4,10 @@ name = gets.chomp
 puts "Введите Ваш рост?"
 height = gets.chomp
 
-height_int = height.to_i
+ideal_weigth = height.to_i - 110
 
-if height_int - 110 > 0
-  puts "#{name}, Ваш идеальный вес #{height_int - 110}"
+if ideal_weigth > 0
+  puts "#{name}, Ваш идеальный вес #{ideal_weigth}"
 else
   puts "#{name}, Ваш вес уже оптимальный!"
 end
