bf8e88681359ca56384ab051e6e3e839999f6b37
Deoshor <39273195+Deoshor@users.noreply.github.com>
2019-08-10 15:37:15 +0300

    upd1

--- less2_ex2.rb
+++ less2_ex2.rb
@@ -1,4 +1,8 @@
 #Заполнить массив числами от 10 до 100 с шагом 5
+range = 10..100
+range.step(5) { |x| puts x}
+
+=begin 
 array = []
 x = 0
 while x < 10 do
@@ -9,3 +13,4 @@ while x <= 100 do
   x += 5
 end
 print array
+=end
