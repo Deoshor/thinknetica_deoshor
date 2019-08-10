dd76e32c16573717ceca1dfb47e84a2dacf9c40c
Deoshor <39273195+Deoshor@users.noreply.github.com>
2019-08-10 16:26:00 +0300

    upd1

--- less2_ex3.rb
+++ less2_ex3.rb
@@ -1,8 +1,8 @@
 #Заполнить массив числами Фибоначчи до 100
 fib = [0, 1]
 i = 0
-while fib.last < 100 do
-  fib_next = fib[i] + fib[i.next]
+while (fib[-1] + fib[-2]) < 100 do
+  fib_next = fib[-1] + fib[-2]
   fib << fib_next
   i += 1
   puts fib[i]
