b754d85e6e2eb267bbfc95d4956a11eb21642c2f
Deoshor <39273195+Deoshor@users.noreply.github.com>
2019-08-11 06:35:21 +0300

    upd2

--- less2_ex2.rb
+++ less2_ex2.rb
@@ -1,16 +1,5 @@
 #Заполнить массив числами от 10 до 100 с шагом 5
 range = 10..100
-range.step(5) { |x| puts x}
-
-=begin 
-array = []
-x = 0
-while x < 10 do
-  x += 5
-end
-while x <= 100 do
-  array.push(x)
-  x += 5
-end
-print array
-=end
+print range.step(5).to_a { |x| puts x}
+#Точно! Нам же массив заполнить нужно, а без перевода range в массив
+#заполняется range.
