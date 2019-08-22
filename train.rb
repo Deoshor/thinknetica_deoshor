f29176c322cba21594a59103d2ff99e9754d0410
Deoshor <39273195+Deoshor@users.noreply.github.com>
2019-08-22 06:10:02 +0300

    upd2

--- train.rb
+++ train.rb
@@ -1,29 +1,26 @@
 require_relative 'station.rb'
 require_relative 'route.rb'
 class Train
-  attr_accessor :number, :type, :wagons
-  attr_reader :route
+  attr_accessor :number
+  attr_reader :route, :speed, :wagons, :type
+
   def initialize(number, type, wagons)
     @number = number
     @type = type
     @wagons = wagons
     @speed = 0
-    @train = []
-    @train << number.to_s
-    @train << type.to_s
-    @train << wagons.to_i
-    @rt = []
+    @pres_stat = []
     @x = 0
   end
-  def train_speedup(speed)
+
+  def speedup(speed)
     @speed += speed.to_i
   end
-  def train_speed
-    @speed
-  end
+
   def train_stop
     @speed = 0
   end
+
   def train_in
     if @speed == 0
       t = @train.at(2)
@@ -33,6 +30,7 @@ class Train
       puts "Остановите поезд, чтобы прицепить вагоны."
     end
   end
+
   def train_out
     if @speed == 0
       t = @train.at(2)
@@ -42,44 +40,50 @@ class Train
       puts "Остановите поезд, чтобы отцепить вагоны."
     end
   end
-  def train_wagons
-    @train.at(2)
-  end
-  def train_route(route)
-    @route = route
-    puts "#{@train.at(0)} на станции #{@route.first}"
-  end
+
   def train_goto(route)
     @route = route
-    if @rt != @route.last
+    if @pres_stat != @route.last
       @x += 1
-      @rt = @route.at(0 + @x.to_i)
-      @rt
+      @pres_stat = @route.at(0 + @x.to_i)
+      @pres_stat
     else
       puts "Поезд №#{@number} на конечной станции"
     end
   end
+
   def train_backto(route)
     @route = route
-    if @rt != @route.first
+    if @pres_stat != @route.first
       @x -= 1
-      @rt = @route.at(0 + @x.to_i)
-      @rt
+      @pres_stat = @route.at(0 + @x.to_i)
+      @pres_stat
     else
       puts "Поезд №#{@number} на начальной станции"
     end
   end
+
   def station_present
-    @rt
+    @pres_stat
   end
+
   def station_next
-    y = @route.index(@rt.to_s)
-    @nrt = @route.at(y + 1)
+    station_index = @route.index(@pres_stat)
+    @route.at(station_index + 1)
     @nrt
   end
+
   def station_last
-    y = @route.index(@rt.to_s)
-    @lrt = @route.at(y - 1)
-    @lrt
+    station_index = @route.index(@pres_stat)
+    @route.at(station_index - 1)
   end
+
 end
+
+
+#@pres_stat - это текущая станция
+#@x - нужен для счетчика итераций, чтобы передвигаться по маршруту
+#В конце при вызове методов определяющих следующую и предыдущую
+#станцию не может быть в индексе -1. Мы же вызываем сам метод, не 
+#не подставляя туда значения индекса. А у метода есть ограничения,
+#за которые он не шагнет.
