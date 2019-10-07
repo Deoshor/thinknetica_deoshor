30b2e905354f6daf610eb97cd6169dd583466951
Deoshor <39273195+Deoshor@users.noreply.github.com>
2019-10-07 06:19:49 +0300

    upd2

--- metaprogramming/accessors.rb
+++ metaprogramming/accessors.rb
@@ -5,29 +5,35 @@ module Accessors
 
   module ClassMethods
     attr_accessor :history
+    attr_reader :name
     def attr_accessor_with_history(*names)
       names.each do |name|
         var_name = "@#{name}".to_sym
-        define_method(name) { instance_variable_get(var_name) }
-
+        
         define_method("#{name}=".to_sym) do |value|
           @history ||= []
+          @history_value ||= {}
           instance_variable_set(var_name, value)
-          @history << instance_variable_get(var_name)
+          @history_value << instance_variable_get(var_name)
+          @history[name] = @history_value[0...-1]
         end
 
         define_method("#{name}_history") do
           @history.reverse.map { |value| puts value }
         end
+        
       end
     end
 
     def strong_attr_accessor(name, type)
       var_name = "@#{name}".to_sym
+      
       define_method(name) { instance_variable_get(var_name) }
+      
       define_method("#{name}=".to_sym) do |value|
         value.class == type ? instance_variable_set(var_name, value) : raise('Incorrect type')
       end
+      
     end
   end
 end
